72796f9a4ba46c5a2698f46fba8c448e0198a961
Felipe Mandu Pereira <felipemandupereira@outlook.com>
2021-01-05 01:54:09 -0300

    criação de modulo web api rest patrimonio-empresa

---
+++ patrimonio-empresa/src/main/java/br/com/navita/patrimonioempresa/PatrimonioEmpresaApplication.java
@@ -0,0 +1,13 @@
+package br.com.navita.patrimonioempresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatrimonioEmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatrimonioEmpresaApplication.class, args);
+	}
+
+}
---
+++ patrimonio-empresa/src/test/java/br/com/navita/patrimonioempresa/PatrimonioEmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.navita.patrimonioempresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatrimonioEmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
