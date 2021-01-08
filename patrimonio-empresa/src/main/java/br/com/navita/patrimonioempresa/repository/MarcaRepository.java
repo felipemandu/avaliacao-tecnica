cf683c696d9d31e2da5bfea1b25f60eacbab5d6e
Felipe Mandu Pereira <felipemandupereira@outlook.com>
2021-01-08 09:56:49 -0300

    atualização dos repositories para utilização de paginação

--- patrimonio-empresa/src/main/java/br/com/navita/patrimonioempresa/repository/MarcaRepository.java
+++ patrimonio-empresa/src/main/java/br/com/navita/patrimonioempresa/repository/MarcaRepository.java
@@ -1,13 +1,16 @@
 package br.com.navita.patrimonioempresa.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.navita.patrimonioempresa.model.Marca;
 
 @Repository
-public interface MarcaRepository extends JpaRepository<Marca, UUID>{
+public interface MarcaRepository extends PagingAndSortingRepository<Marca, UUID>{
+
+	Optional<Marca> findByNome(String nome);
 
 }
--- patrimonio-empresa/src/main/java/br/com/navita/patrimonioempresa/repository/PatrimonioRepository.java
+++ patrimonio-empresa/src/main/java/br/com/navita/patrimonioempresa/repository/PatrimonioRepository.java
@@ -2,12 +2,29 @@ package br.com.navita.patrimonioempresa.repository;
 
 import java.util.UUID;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.navita.patrimonioempresa.model.Patrimonio;
 
 @Repository
-public interface PatrimonioRepository extends JpaRepository<Patrimonio, UUID>{
+public interface PatrimonioRepository extends PagingAndSortingRepository<Patrimonio, UUID> {
+	
+	@Query("select p from Patrimonio p join p.marcaId m where m.nome = :nome")
+	Page<Patrimonio> findByMarcaNome(@Param("nome") String nome, Pageable page);
+
+	@Query("select p from Patrimonio p join p.marcaId m where m.marcaId = :id")
+	Page<Patrimonio> findByMarcaId(@Param("id") UUID id, Pageable page);
+
+	@Query("select p from Patrimonio p join p.marcaId m where m.nome LIKE %:nome%")
+	Page<Patrimonio> findByMarcaNomeLike(@Param("nome") String nome, Pageable page);
+
+	@Query("select p from Patrimonio p where p.descricao LIKE %:descricao%")
+	Page<Patrimonio> findByDescricaoLike(@Param("descricao") String descricao, Pageable page);
+	
 
 }
