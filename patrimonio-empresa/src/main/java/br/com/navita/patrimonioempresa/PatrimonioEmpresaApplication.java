ef84dc4f8b85678fcac678c650608eae150aa7d1
Felipe Mandu Pereira <felipemandupereira@outlook.com>
2021-01-08 10:21:32 -0300

    configuração do metadados do swagger

--- patrimonio-empresa/src/main/java/br/com/navita/patrimonioempresa/PatrimonioEmpresaApplication.java
+++ patrimonio-empresa/src/main/java/br/com/navita/patrimonioempresa/PatrimonioEmpresaApplication.java
@@ -3,6 +3,10 @@ package br.com.navita.patrimonioempresa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
 @EnableCaching
@@ -10,6 +14,14 @@ public class PatrimonioEmpresaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PatrimonioEmpresaApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI springDocOpenAPI() {
+		return new OpenAPI()
+				.info(new Info().title("Patrimonio Empresa")
+				.description("Web Api Rest para o gerenciamenento de um empresa")
+				.version("v0.0.1"));
 	}
 
 }
